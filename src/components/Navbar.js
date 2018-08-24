56b842d793eed43a418e823f962bf52522312313
Carter McAlister <carterm126@gmail.com>
2018-08-24 16:47:45 -0500

    Init Project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,36 @@ import Link from 'gatsby-link'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
+//my stuff
+import navbarStyles from "./navbar.module.css"
+import globalStyles from "./global.module.css"
+
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
+  <nav className={navbarStyles.navbar}>
+    <div className={navbarStyles.navbarContainer}>
+        <Link to="/" className={navbarStyles.navbarBrand} title="Carter McAlister">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={""} alt="Kaldi" style={{ width: '88px' }} />
           </figure>
+          Carter McAlister 
+          <span className={navbarStyles.brandTagline}>
+            <span className={navbarStyles.tagLineSeperator}> / </span>
+            Full-Stack Developer
+          </span>
         </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
+      <div className={navbarStyles.navGroup}>
+        <Link className={navbarStyles.navItem} to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+        <Link className={navbarStyles.navItem} to="/projects">
+          Projects
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+        <Link className={navbarStyles.navItem} to="/blog">
+          Blog
+        </Link>
+        <Link className={navbarStyles.navItem} to="/contact">
+          Contact
+        </Link>
       </div>
     </div>
   </nav>
