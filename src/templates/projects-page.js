76f4b29a14089b3ebe6faf4a8dcc2ebabbc76652
Carter McAlister <carterm126@gmail.com>
2018-08-29 16:55:39 -0500

    Blog Page & Projects Page Created

---
+++ src/templates/projects-page.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Content, { HTMLContent } from '../components/Content'
+
+export const ProjectsPageTemplate = ({ title, content, contentComponent }) => (
+  <section className="section section--gradient">
+    <div className="container">
+      <div className="columns">
+        <div className="column is-10 is-offset-1">
+          <div className="section">
+            <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              {title}
+            </h2>
+            <PageContent className="content" content={content} />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+ProjectsPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const ProjectsPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <ProjectsPageTemplate
+      contentComponent={HTMLContent}
+      title={post.frontmatter.title}
+      content={post.html}
+    />
+  )
+}
+
+ProjectsPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default ProjectsPage
+
+export const ProjectsPageQuery = graphql`
+  query ProjectsPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
